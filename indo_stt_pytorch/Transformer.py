48c10b3a2f0de29d38ebe21f4dd39e2105300ba9
Kevin Putra Santoso <92637327+echelon2718@users.noreply.github.com>
2023-08-23 18:05:11 +0700

    Update Transformer.py

--- indo_stt_pytorch/Transformer.py
+++ indo_stt_pytorch/Transformer.py
@@ -13,45 +13,42 @@ class GELU(nn.Module):
         return x
 
 class TokenEmbedding(nn.Module):
-    def __init__(self, num_vocab=30, num_hid=64):
+    def __init__(self, num_vocab=30, num_hid=64, maxlen=255): # If maxlen changed, change the attn_mask dim as well
         super().__init__()
-        self.num_vocab = num_vocab
-        self.num_hid = num_hid
+        self.maxlen = maxlen
+        self.emb = nn.Embedding(num_vocab, num_hid)
+        self.pos_emb = nn.Embedding(maxlen, num_hid)
 
     def forward(self, x):
-        maxlen = x.shape[-1]
-        pos = torch.arange(0, maxlen, 1)
-        emb = nn.Embedding(self.num_vocab, self.num_hid)(x)
-        pos_emb = nn.Embedding(maxlen, self.num_hid)(x)
+        pos = torch.arange(0, self.maxlen, 1).to("cuda:0")
+        emb = self.emb(x.to("cuda:0"))
+        pos_emb = self.pos_emb(pos)
         out = emb + pos_emb
-        return out.to(device)
+        return out
 
 class SpeechFeatureEmbedding(nn.Module):
-    def __init__(self, num_hid=64, maxlen=100):
+    def __init__(self, num_hid=64):
         super().__init__()
         self.conv1 = nn.Conv1d(in_channels=num_hid, out_channels=num_hid, kernel_size=11, stride=2, padding=5)
         self.conv2 = nn.Conv1d(in_channels=num_hid, out_channels=num_hid, kernel_size=11, stride=2, padding=5)
         self.conv3 = nn.Conv1d(in_channels=num_hid, out_channels=num_hid, kernel_size=11, stride=2, padding=5)
-        self.admaxpool1 = nn.AdaptiveMaxPool1d(1024)
-        self.admaxpool2 = nn.AdaptiveMaxPool1d(512)
         self.gelu = GELU()
 
     def forward(self, x):
-        x = self.conv1(x.T)
+        x = self.conv1(x.permute(0, 2, 1))
         x = self.gelu(x)
-        x = self.admaxpool1(x)
         x = self.conv2(x)
         x = self.gelu(x)
         x = self.conv3(x)
         x = self.gelu(x)
-        return x.T
+        return x.permute(0, 2, 1)
 
 class TransformerEncoder(nn.Module):
     def __init__(self, embed_dim, num_heads, feed_forward_dim, rate=0.1):
         super(TransformerEncoder, self).__init__()
-        self.att = nn.MultiheadAttention(embed_dim, num_heads)
+        self.att = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         self.ffn = nn.Sequential(
-            nn.Linear(embed_dim, feed_forward_dim),
+            nn.Linear(embed_dim, feed_forward_dim),     
             GELU(),
             nn.Linear(feed_forward_dim, embed_dim)
         )
@@ -75,8 +72,8 @@ class TransformerDecoder(nn.Module):
         self.layernorm1 = nn.LayerNorm(embed_dim, eps=1e-6)
         self.layernorm2 = nn.LayerNorm(embed_dim, eps=1e-6)
         self.layernorm3 = nn.LayerNorm(embed_dim, eps=1e-6)
-        self.self_att = nn.MultiheadAttention(embed_dim, num_heads)
-        self.enc_att = nn.MultiheadAttention(embed_dim, num_heads)
+        self.self_att = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
+        self.enc_att = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         self.self_dropout = nn.Dropout(0.5)
         self.enc_dropout = nn.Dropout(0.1)
         self.ffn_dropout = nn.Dropout(0.1)
@@ -90,10 +87,10 @@ class TransformerDecoder(nn.Module):
         return torch.triu(torch.full((size, size), dtype, device=device), diagonal=1)
 
     def forward(self, enc_out, target):
-        causal_mask = self.causalAttentionMask(size=target.shape[0], dtype=float('-inf'), device=target.device)
+        causal_mask = self.causalAttentionMask(size=255, dtype=float('-inf'), device=target.device)
         target_att = self.self_att(target, target, target, attn_mask=causal_mask, is_causal=True)[0]
         target_norm = self.layernorm1(target + self.self_dropout(target_att))
-        enc_out = self.enc_att(target_norm, target_norm, enc_out)[0]
+        enc_out = self.enc_att(target_norm, enc_out, enc_out)[0]
         enc_out_norm = self.layernorm2(self.enc_dropout(enc_out) + target_norm)
         ffn_out = self.ffn(enc_out_norm)
         ffn_out_norm = self.layernorm3(enc_out_norm + self.ffn_dropout(ffn_out))
@@ -117,7 +114,7 @@ class Transformer(nn.Module):
         self.target_maxlen = target_maxlen
         self.num_classes = num_classes
 
-        self.enc_input = SpeechFeatureEmbedding(num_hid=num_hid, maxlen=source_maxlen)
+        self.enc_input = SpeechFeatureEmbedding(num_hid=num_hid)
         self.dec_input = TokenEmbedding(num_vocab=num_classes, num_hid=num_hid)
 
         self.encoder = nn.Sequential(
